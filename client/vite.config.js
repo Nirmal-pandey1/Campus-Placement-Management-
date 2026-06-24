02b253e3e56c1efa7155ed991255fab6e564f79a
nirmal <nirmalpandey680@gmail.com>
2026-06-24 18:24:54 +0530

    vite proxy change

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://campus-placement-management-0cwl.onrender.com",
         changeOrigin: true,
       },
     },
